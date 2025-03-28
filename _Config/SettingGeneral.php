2944b865da7c94c181b16ded8126faae21d9d0fa
dhiforester <dhiforester@gmail.com>
2025-03-29 03:03:37 +0700

    Inisialisasi : Commite Pertama Kali

---
+++ _Config/SettingGeneral.php
@@ -0,0 +1,11 @@
+<?php
+    $title_page="Husnul Khotimah";
+    $kata_kunci="Husnul Khotimah, SDM, Topsis";
+    $deskripsi="Aplikasi Penilaian Kinerja Karyawan Menggunakan Algoritma Topsis";
+    $alamat_bisnis="Jl. Manis Kidul-Sayana No.84, Maniskidul, Kec. Jalaksana, Kabupaten Kuningan, Jawa Barat 45554";
+    $email_bisnis="husnul_khotimah@gmail.com";
+    $telepon_bisnis="0232-613808";
+    $favicon="husnul.png";
+    $logo="husnul.png";
+    $base_url="http://localhost:81/Husnul/";
+?>
